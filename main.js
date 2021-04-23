79129e7bdbe072326b87621de7707cf422ce1b86
SaharshRajput <73647463+SaharshRajput@users.noreply.github.com>
2021-04-23 17:45:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ menu_list_array=["Chicken Tandoori Pizza",
              function getmenu()
              {
                  var htmldata;
-                 htmldata="<ol class='menulist'>";
+                 htmldata="<ol class='menulist'>"
                  menu_list_array.sort();
                  for(var i=0;i<menu_list_array.length;i++){
                      htmldata=htmldata+"<li>"+menu_list_array[i]+"</li>"
                  }
-                 htmldata=htmldata="</ol>";
-                 document.getElementById("displaymenu").innerHTML=htmldata;
+                 htmldata=htmldata+"</ol>"
+                 document.getElementById("display_menu").innerHTML=htmldata;
              }
 
              function add_item(){
@@ -21,11 +21,11 @@ menu_list_array=["Chicken Tandoori Pizza",
                  var item=document.getElementById("add_item").value;
                  menu_list_array.push(item);
                  menu_list_array.sort();
-                 htmldata="<section class='cards'>";
-                 menu_list_array.sort();
+                 htmldata="<section class='cards'>"
+                
                  for(var i=0;i<menu_list_array.length;i++){
                      htmldata=htmldata+"<div class='card'>"+"<img src='pizzaImg.png'/>"+menu_list_array[i]+"</div>";
                  }
-                 htmldata=htmldata="</section>";
+                 htmldata=htmldata+"</section>"
                  document.getElementById("display_addedmenu").innerHTML=htmldata;
              }
